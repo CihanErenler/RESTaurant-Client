7ee328e98a5d9a9628889bef485c6bab1940d04b
rhexa <akun.kripitit@gmail.com>
2021-10-06 20:27:55 +0300

    added icon to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,84 +9,67 @@ import colors from "../helpers/colors";
  * @description text: button text
  * @description onPress: function to be invoked onPress event
  * @description model: 'primary'
+ * @description icon: the whole icon component. Ex. <AntDesign name='heart'... /> 
  * @description style: style for the button container (TouchableOpacity)
  * @description textStyle: {color, fontSize, fontWeight, fontStyle}
  * @returns
  */
 const Button = (props) => {
-  const { text, model, onPress, style, textStyle } = props;
+  const { text, model, onPress, style, textStyle, icon } = props;
+
+  const primary = () => (
+    <TouchableOpacity onPress={onPress}>
+        <LinearGradient
+          colors={["#FF7E6B", "#FFBD59"]}
+          start={[0.1, 0.9]}
+          end={[0.9, 0.1]}
+          locations={[0.1, 0.9]}
+          style={{...styles.gradient, ...style}}
+        >
+          <View style={styles.textContainer} >
+            <Text
+              style={{
+                color: colors.bg_white,
+                ...styles.buttonText,
+                ...textStyle,
+              }}
+            >
+              {text}
+            </Text>
+            {icon && icon}
+          </View>
+        </LinearGradient>
+    </TouchableOpacity>
+  )
 
   switch (model) {
     case "primary":
-      return (
-        <TouchableOpacity onPress={onPress} style={style}>
-          <View style={styles.button}>
-            <LinearGradient
-              colors={["#FF7E6B", "#FFBD59"]}
-              start={[0.1, 0.9]}
-              end={[0.9, 0.1]}
-              locations={[0.1, 0.9]}
-              style={styles.gradient}
-            >
-              <Text
-                style={{
-                  color: colors.bg_white,
-                  ...styles.buttonText,
-                  ...textStyle,
-                }}
-              >
-                {text}
-              </Text>
-            </LinearGradient>
-          </View>
-        </TouchableOpacity>
-      );
-      break;
+      return primary()
+      break
 
     default:
-      return (
-        <TouchableOpacity onPress={onPress} style={style}>
-          <View style={styles.button}>
-            <LinearGradient
-              colors={["#FF7E6B", "#FFBD59"]}
-              start={[0.1, 0.9]}
-              end={[0.9, 0.1]}
-              locations={[0.1, 0.9]}
-              style={styles.gradient}
-            >
-              <Text
-                style={{
-                  color: colors.bg_white,
-                  ...styles.buttonText,
-                }}
-              >
-                {text}
-              </Text>
-            </LinearGradient>
-          </View>
-        </TouchableOpacity>
-      );
-      break;
+      return primary()
+      break
   }
 };
 
 export default Button;
 
 const styles = StyleSheet.create({
-  button: {
-    height: 45,
-    width: 300,
-    overflow: "hidden",
-    borderRadius: 45,
-  },
   buttonText: {
     fontWeight: "700",
     fontSize: 16,
   },
   gradient: {
-    height: "100%",
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
+    height: 45,
+    width: 300,
+    overflow: "hidden",
+    borderRadius: 45,
   },
+  textContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  }
 });
