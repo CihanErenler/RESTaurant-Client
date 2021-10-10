ffdf0818e695d8c5f14df587b9219e09037f7082
cihanerenler <cihanerenler@outlook.com>
2021-10-10 13:08:51 +0300

    fetch functions added

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
-const base_url = "https://api.yelp.com/v3/businesses";
-const apiKey =
-  "9MqBJIOqNn_zy3vOm8tz-B3f9xKL_GRSipoaJ7FOuEB8bxi_N9HEPW9pSTviGD1HSD4JJUlo5XBSqmnlytotRgdg3TsA5akH_4nnUnYmjUIEtMuLig9JW9FHe-NSYXYx";
+const base_url = process.env.BASE_URL;
+const apiKey = process.env.API_KEY;
+const db_url = process.env.DB_URL;
 
 export default getRest = {
   // Get data
@@ -33,14 +33,80 @@ export default getRest = {
     return data;
   },
 
-  getByCoordinate: async ({latitude, longitude}) => {
-    const radius = 3000
-    const categories = 'food'
-    const url = `${base_url}/search?latitude=${latitude}&longitude=${longitude}&radius=${radius}&categories=${categories}`
+  // Get coordinate
+  getByCoordinate: async ({ latitude, longitude }) => {
+    const radius = 3000;
+    const categories = "food";
+    const url = `${base_url}/search?latitude=${latitude}&longitude=${longitude}&radius=${radius}&categories=${categories}`;
     const response = await fetch(url, {
       headers: { Authorization: "Bearer " + apiKey },
-    })
+    });
     const data = await response.json();
     return data;
-  }
+  },
+
+  // Get liked restaurant list
+  getLikedRest: async (token) => {
+    const data = await fetch(db_url, {
+      method: "GET",
+      headers: {
+        auth_token: token,
+      },
+    });
+    const list = await data.json();
+    return list;
+  },
+
+  // Post restaurant
+  addLiked: async (liked, token) => {
+    const response = await fetch(`${db_url}/liked`, {
+      method: "POST",
+      headers: {
+        auth_token: token,
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(liked),
+    });
+    const data = await response.json();
+    return data;
+  },
+
+  // Register user
+  registerUser: async (user) => {
+    const response = await fetch(`${db_url}/user/register`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const data = await response.json();
+    return data;
+  },
+
+  // Login user
+  loginUser: async (user) => {
+    const response = await fetch(`${db_url}/user/login`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    const data = await response.json();
+    return data;
+  },
+
+  // Delete restaurant by id
+  deleteLiked: async (id, token) => {
+    const response = await fetch(`${db_url}/liked/${id}`, {
+      method: "DELETE",
+      headers: {
+        auth_token: token,
+        "Content-type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data;
+  },
 };
